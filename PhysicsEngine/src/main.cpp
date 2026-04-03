dbef9fa13d7b25a84ea7acb7ebb1d0009823a3dc
Cole-Lowery <CLowery@student.neumont.edu>
2026-04-03 12:19:36 -0600

    Organizing file structure

--- PhysicsEngine/src/main.cpp
+++ PhysicsEngine/src/main.cpp
@@ -1,31 +1,72 @@
 #include "raylib.h"
+#include "raymath.h"
 #include "resource_dir.h"
+#include "Body.h"
+#include "World.h"
+#include "Random.h"
 
-int main ()
+int main()
 {
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
-	InitWindow(1280, 800, "Hello Raylib");
-
-	SearchAndSetResourceDir("resources");
-
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-
-		ClearBackground(BLACK);
-
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		DrawTexture(wabbit, 400, 200, WHITE);
-
-		EndDrawing();
-	}
-
-	UnloadTexture(wabbit);
-
-	CloseWindow();
-	return 0;
+    // Initialize world
+    World world;
+    
+    SetRandomSeed(5);
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    InitWindow(1280, 800, "Hello Raylib");
+    SearchAndSetResourceDir("resources");
+    
+    Texture wabbit = LoadTexture("wabbit_alpha.png");
+    
+    // Game loop
+    while (!WindowShouldClose())
+    {
+        float deltaTime = GetFrameTime();
+        
+        // Input: Spawn bodies on left click
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            Body body = { 0 };
+            body.position = GetMousePosition();
+            
+            float angle = GetRandomFloat() * (2 * PI);
+            Vector2 direction;
+            direction.x = cosf(angle);
+            direction.y = sinf(angle);
+            
+            body.velocity = Vector2Scale(direction, GetRandomFloat(300.0f));
+            body.acceleration = Vector2{ 0, 0 };
+            body.size = (float)GetRandomValue(5, 20);
+            body.restitution = 1.0f;
+            body.mass = body.size * 10;
+            
+            world.AddBody(body);
+        }
+        
+        // Input: Apply attraction force on right click
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            Vector2 position = GetMousePosition();
+            world.ApplyAttraction(position, 100.0f, 900000.0f);
+            DrawCircleLinesV(position, 100, GREEN);
+        }
+        
+        // Update physics
+        world.Step(deltaTime);
+        
+        // Draw
+        BeginDrawing();
+        ClearBackground(BLACK);
+        
+        DrawText("Hello Raylib", 200, 200, 20, WHITE);
+        DrawTexture(wabbit, 400, 200, WHITE);
+        
+        world.Draw();
+        
+        EndDrawing();
+    }
+    
+    UnloadTexture(wabbit);
+    CloseWindow();
+    
+    return 0;
 }
